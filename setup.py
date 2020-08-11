bbada9f4220e4f4172432df1f16310e82403eb05
Nathalie Willems <nathalie.willems@ibm.com>
2020-08-11 11:51:01 +1000

    added logger to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
 
-    name='oct_converter', 
+    name='oct_converter',
     version='0.1',
-    description='Extract OCT and fundus data from proprietary file formats.',  
+    description='Extract OCT and fundus data from proprietary file formats.',
     url='https://github.com/marksgraham/OCT-Converter',
     author='Mark Graham',
     author_email='markgraham539@gmail.com',
     python_requires='>3.5',
+    packages = find_packages(),
+    scripts = ['setup_logger.py'],
     install_requires=['construct','imageio','natsort','numpy','opencv-python','pydicom','six','matplotlib','imageio-ffmpeg', 'pylibjpeg']
 
 )
